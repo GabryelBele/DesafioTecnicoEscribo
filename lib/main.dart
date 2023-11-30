3f2cdd3597ee8bbaafca25ccc23ea613b9641d80
Gabryel Bele <belegabryel19@gmail.com>
2023-11-30 19:53:50 -0300

    "update main, add example two"

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'minha_funcao.dart';
 void main() {
   int numero1 = 10;
   int resultado1 = somatorioDivisiveis(numero1);
-  int numero2 = 10;
+  int numero2 = 11;
   int resultado2 = somatorioDivisiveis(numero2);
   print(
       "O somatório dos números menores que $numero1 e divisíveis por 3 ou 5 é: $resultado1");
